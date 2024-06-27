87bb7a4966bc0c4b0b5fba662c3bcf0873a3e843
Chance Mark <chance8476@outlook.com>
2024-06-28 01:51:16 +0900

    Update ERC20Token.sol

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -2,18 +2,67 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ERC20Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialSupply);
+contract MyToken is ERC20Capped, ERC20Burnable {
+    address payable public owner;
+    uint256 public blockReward;
+
+    constructor(
+        uint256 cap,
+        uint256 reward
+    ) ERC20("MyToken", "MYT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
+        _mint(owner, 50000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals()); // Setting block reward for first deploy
     }
 
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+    // Setting miner reward
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
     }
 
-    function buy() external payable {
-        require(msg.value > 0, "You must send some ether");
-        _mint(msg.sender, msg.value * 10 ** decimals() /price);
+    // block.conbase validation for rewarding the minder; prevents miner from manipulating teh token
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        if (
+            from != block.coinbase &&
+            to != block.coinbase &&
+            block.coinbase != address(0)
+        ) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function _mint(
+        address account,
+        uint256 amount
+    ) internal virtual override(ERC20Capped, ERC20) {
+        require(
+            ERC20.totalSupply() + amount <= cap(),
+            "ERC20Capped: cap exceeded"
+        );
+        super._mint(account, amount);
+    }
+
+      //Destroying the contract
+    function destroyContract() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    // Set block rewards
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
+    }
+
+    //reusable modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 }
