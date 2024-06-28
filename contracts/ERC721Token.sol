b3d630819bd631ff53b3ecfaf351a41165516bb0
Chance Mark <chance8476@outlook.com>
2024-06-29 00:08:03 +0900

    Update ERC721Token.sol

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/drafts/Counters.sol";
 
-contract ERC721Token is ERC721 {
-    uint256 public nextTokenId;
-    
-    constructor() ERC721("MyNFT", "MNFT") {}
+contract ERC721Token is ERC721Full {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    function mint() external {
-        _safeMint(msg.sender, nextTokenId);
-        nextTokenId++;
+    constructor() ERC721Full("GameItem", "ITM") public {
+    }
+
+    function awardItem(address player, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 }
